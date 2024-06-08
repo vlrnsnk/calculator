6003ca039c776569fc5f303c57a272a2d2f438ab
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-06-08 13:33:05 +0200

    Add operator function to handle operations on two numbers

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
 
+const operate = (firstNumber, secondNumber, operator) => {
+  switch (operator) {
+    case '+':
+      return add(firstNumber, secondNumber);
+    case '-':
+      return subtract(firstNumber, secondNumber);
+    case '*':
+      return multiply(firstNumber, secondNumber);
+    case '/':
+      return divide(firstNumber, secondNumber);
+  }
+};
