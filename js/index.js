2ffe933f34adb113a92975ccda6cc71a91967e3e
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-06-14 00:14:57 +0200

    Rearrange project file structure

--- js/index.js
+++ js/index.js
@@ -1,28 +1,8 @@
+import {operate} from "./math.js";
+
 let firstNumber;
 let secondNumber;
 let operator;
-
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-const multiply = (a, b) => a * b;
-
-const divide = (a, b) => a / b;
-
-const operate = (firstNumber, secondNumber, operator) => {
-  switch (operator) {
-    case '+':
-      return add(firstNumber, secondNumber);
-    case '-':
-      return subtract(firstNumber, secondNumber);
-    case '*':
-      return multiply(firstNumber, secondNumber);
-    case '/':
-      return divide(firstNumber, secondNumber);
-  }
-};
-
 let displayValue = '0';
 const displayEl = document.querySelector('.display__lower');
 displayEl.textContent = displayValue;
---
+++ js/math.js
@@ -0,0 +1,22 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const operate = (firstNumber, secondNumber, operator) => {
+  switch (operator) {
+    case '+':
+      return add(firstNumber, secondNumber);
+    case '-':
+      return subtract(firstNumber, secondNumber);
+    case '*':
+      return multiply(firstNumber, secondNumber);
+    case '/':
+      return divide(firstNumber, secondNumber);
+  }
+};
+
+export { operate };
