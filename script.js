89f3373e295ee358f405844709a78774e88c6bb6
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-06-13 22:30:48 +0200

    Make calculator work

--- script.js
+++ script.js
@@ -41,3 +41,35 @@ const onClickNumberBtn = (event) => {
 Array.from(numberBtnList).map(
   (numberBtn) => numberBtn.addEventListener('click', onClickNumberBtn)
 );
+
+const operatorBtnList = document.querySelectorAll('.btn--operator');
+
+const onClickOperatorBtn = (event) => {
+  if (!firstNumber) {
+    firstNumber = Number(displayValue);
+    displayValue = '';
+    operator = event.target.textContent;
+  } else {
+    secondNumber = Number(displayValue);
+    operator = event.target.textContent;
+    displayValue = operate(firstNumber, secondNumber, operator);
+    displayEl.textContent = displayValue;
+  }
+};
+
+Array.from(operatorBtnList).map(
+  (operatorBtn) => operatorBtn.addEventListener('click', onClickOperatorBtn)
+);
+
+const onClickCalculateBtn = () => {
+  secondNumber = Number(displayValue);
+  displayValue = operate(firstNumber, secondNumber, operator);
+  displayEl.textContent = displayValue;
+  firstNumber = '';
+  secondNumber = '';
+  displayValue = '';
+};
+
+const calculateBtn = document.querySelector('.btn--calculate');
+
+calculateBtn.addEventListener('click', onClickCalculateBtn);
