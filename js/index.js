992cf033971bd568addd98a6eb4340211df5904a
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-06-14 15:12:04 +0200

    Rebase

--- js/index.js
+++ js/index.js
@@ -1,85 +1,78 @@
-import { operate } from "./math.js";
-import { getDisplayValue, setDisplayValue } from "./ui.js";
+import {operate} from "./math.js";
 
 let firstNumber;
 let secondNumber;
 let operator;
-setDisplayValue('0');
+let displayValue = '0';
+const displayEl = document.querySelector('.display__lower');
+displayEl.textContent = displayValue;
 
 const numberBtnList = document.querySelectorAll('.btn--number');
-const onClickNumberBtn = (event) => {
-  const displayValue = getDisplayValue();
-
-  if (displayValue === '0') {
-    setDisplayValue(event.target.dataset.value);
-  } else if (firstNumber) {
-    setDisplayValue(event.target.dataset.value);
-  } else if (displayValue.length < 10) {
-    setDisplayValue(displayValue + event.target.dataset.value);
-  }
-}
-
 Array.from(numberBtnList).map(
   (numberBtn) => numberBtn.addEventListener('click', onClickNumberBtn)
 );
 
 const operatorBtnList = document.querySelectorAll('.btn--operator');
-
-const onClickOperatorBtn = (event) => {
-  const displayValue = getDisplayValue();
-
-  if (!firstNumber) {
-    firstNumber = Number(displayValue);
-    operator = event.target.dataset.value;
-  } else {
-    secondNumber = Number(displayValue);
-    operator = event.target.dataset.value;
-    setDisplayValue(
-      operate(firstNumber, secondNumber, operator),
-    );
-  }
-};
-
 Array.from(operatorBtnList).map(
   (operatorBtn) => operatorBtn.addEventListener('click', onClickOperatorBtn)
 );
 
-const onClickCalculateBtn = () => {
-  secondNumber = Number(getDisplayValue());
-  setDisplayValue(
-    operate(firstNumber, secondNumber, operator),
-  );
-  firstNumber = '';
-  secondNumber = '';
-};
-
 const calculateBtn = document.querySelector('.btn--calculate');
-
 calculateBtn.addEventListener('click', onClickCalculateBtn);
 
-const onClickAllClearBtn = () => {
+const allClearBtn = document.querySelector('.btn--all-clear');
+allClearBtn.addEventListener('click', onClickAllClearBtn);
+
+const deleteBtn = document.querySelector('.btn--delete');
+deleteBtn.addEventListener('click', onClickDeleteBtn);
+
+// event handlers
+function onClickNumberBtn(event){
+  if (displayEl.textContent === '0') {
+    displayValue = event.target.dataset.value;
+  } else if (displayValue.length < 10) {
+    displayValue += event.target.dataset.value;
+  }
+
+  displayEl.textContent = displayValue;
+}
+
+function onClickOperatorBtn(event) {
+  if (!firstNumber) {
+    firstNumber = Number(displayValue);
+    displayValue = '';
+    operator = event.target.dataset.value;
+  } else {
+    secondNumber = Number(displayValue);
+    operator = event.target.dataset.value;
+    displayValue = operate(firstNumber, secondNumber, operator);
+    displayEl.textContent = displayValue;
+  }
+}
+
+function onClickCalculateBtn() {
+  secondNumber = Number(displayValue);
+  displayValue = operate(firstNumber, secondNumber, operator);
+  displayEl.textContent = displayValue;
+  firstNumber = '';
+  secondNumber = '';
+  displayValue = '';
+}
+
+function onClickAllClearBtn() {
   firstNumber = '';
   secondNumber = '';
   operator = '';
-  setDisplayValue('0')
-};
+  displayValue = '0';
+  displayEl.textContent = displayValue;
+}
 
-const allClearBtn = document.querySelector('.btn--all-clear');
-
-allClearBtn.addEventListener('click', onClickAllClearBtn);
-
-const onClickDeleteBtn = () => {
-  const displayValue = getDisplayValue();
-
+function onClickDeleteBtn() {
   if (displayValue.length > 1 && displayValue !== '0') {
-    setDisplayValue(
-      displayValue.slice(0, -1),
-    );
+    displayValue = displayValue.slice(0, -1);
+    displayEl.textContent = displayValue;
   } else {
-    setDisplayValue('0')
+    displayValue = '0';
+    displayEl.textContent = displayValue;
   }
-};
-
-const deleteBtn = document.querySelector('.btn--delete');
-
-deleteBtn.addEventListener('click', onClickDeleteBtn);
+}
